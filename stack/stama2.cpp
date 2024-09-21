2203642fcfb2a05fa6c23ca790925826b6f89783
rafid <rafidadib123@gmail.com>
2024-09-21 20:13:34 +0600

    version 2
    - first time
    
    - added some new features

---
+++ stack/stama2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "hrasdasd";
+    cout << endl;
+    cout << "stamina";
+    cout << endl;
+    cout << "rafid";
+    cout << endl;
+    cout << "new";
+}
