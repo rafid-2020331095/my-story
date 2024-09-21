e60563ab965d409495fef7797920d21851a21c8b
rafid-2020331095 <106920440+rafid-2020331095@users.noreply.github.com>
2024-09-21 23:13:27 +0600

    Create newra.cpp
    yeah thats first description commit

---
+++ newra.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "hrasdasd";
+    cout << endl;
+    cout << "can u hear me";
+    cout << endl;
+    cout << "changed";
+  cout<<"yes";
+}
